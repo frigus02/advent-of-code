23a2584ca7805e88c696a0f057a5e5dbda182928
Jan Kühle <jkuehle90@gmail.com>
2021-01-08 11:42:16 +0100

    Day 3, part 2

--- 2020/03-toboggan-trajectory/src/main.rs
+++ 2020/03-toboggan-trajectory/src/main.rs
@@ -16,20 +16,26 @@ fn main() -> Result<(), BoxError> {
     let map_width = map[0].len();
     let map_height = map.len();
 
-    let slope = (3, 1);
-    let mut col = 0;
-    let mut row = 0;
-    let mut trees = 0;
-    loop {
-        col = (col + slope.0) % map_width;
-        row += slope.1;
-        if row < map_height {
-            trees += map[row][col];
-        } else {
-            break;
+    let slopes = &[(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let mut result: u64 = 1;
+    for slope in slopes {
+        let mut col = 0;
+        let mut row = 0;
+        let mut trees = 0;
+        loop {
+            col = (col + slope.0) % map_width;
+            row += slope.1;
+            if row < map_height {
+                trees += map[row][col];
+            } else {
+                break;
+            }
         }
+
+        println!("Right {}, down {}: {}", slope.0, slope.1, trees);
+        result *= trees;
     }
 
-    println!("{}", trees);
+    println!("{}", result);
     Ok(())
 }

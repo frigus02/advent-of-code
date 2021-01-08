e097356d46d2ec8862ee1bafba8186225e1e17fa
Jan Kühle <jkuehle90@gmail.com>
2021-01-08 15:57:08 +0100

    Day 6, part 2

--- 2020/06-custom-customs/src/main.rs
+++ 2020/06-custom-customs/src/main.rs
@@ -1,25 +1,34 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::io::{self, BufRead as _};
 
 type BoxError = Box<dyn std::error::Error>;
 
 fn main() -> Result<(), BoxError> {
-    let mut group = HashSet::new();
+    let mut group_answers = HashMap::new();
+    let mut group_people = 0;
     let mut total = 0;
     for line in io::stdin().lock().lines() {
         let line = line?;
         if line.is_empty() {
-            total += group.len();
-            group.clear();
+            total += group_answers
+                .drain()
+                .filter(|(_, v)| *v == group_people)
+                .count();
+            group_people = 0;
             continue;
         }
 
+        group_people += 1;
         for answer in line.chars() {
-            group.insert(answer);
+            let count = group_answers.entry(answer).or_insert(0);
+            *count += 1;
         }
     }
 
-    total += group.len();
+    total += group_answers
+        .drain()
+        .filter(|(_, v)| *v == group_people)
+        .count();
 
     println!("{}", total);
     Ok(())

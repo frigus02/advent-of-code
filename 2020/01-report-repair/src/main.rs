8ecf24d3de3caf7f802587a0ee5793bd7da5a496
Jan Kühle <jan.kuehle@codecentric.de>
2021-01-08 10:43:22 +0100

    Day 1, part 2

--- 2020/01-report-repair/src/main.rs
+++ 2020/01-report-repair/src/main.rs
@@ -1,18 +1,23 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::io::{self, BufRead as _};
 
 type BoxError = Box<dyn std::error::Error>;
 
 fn main() -> Result<(), BoxError> {
-    let mut seen = HashSet::new();
+    let mut one = HashSet::new();
+    let mut two = HashMap::new();
     for line in io::stdin().lock().lines() {
         let n: u32 = line?.parse()?;
         let need = 2020 - n;
-        if seen.contains(&need) {
-            println!("{} * {} = {}", n, need, n * need);
+        if let Some((a, b)) = two.get(&need) {
+            println!("{} * {} * {} = {}", a, b, n, a * b * n);
             return Ok(());
         } else {
-            seen.insert(n);
+            for a in &one {
+                two.insert(*a + n, (*a, n));
+            }
+
+            one.insert(n);
         }
     }
 
